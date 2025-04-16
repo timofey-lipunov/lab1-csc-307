618464f4b4f1d3b31d2e8b3463bf292cbb9bfccc
timofeylipunov <tlipunov21@gmail.com>
2025-04-15 20:17:38 -0700

    IA3 done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,19 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
+  function deleteUser(id) {
+    const promise = fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE",
+    });
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, []);
+
+  function removeOneCharacter(index) {
+    const id = characters[index].id;
+    deleteUser(id)
+      .then((response) => {
+        if (response.status === 204) {
+          const updated = characters.filter((character, i) => {
+            return i !== index;
+          });
+          setCharacters(updated);
+        } else if (response.status === 404) {
+          console.log("User not found");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function updateList(person) { 
+    postUser(person)
+      .then((response) => {
+        if (response.status === 201) {
+          return response.json();
+        }
+        throw new Error("Failed to create user");
+      })
+      .then((newUser) => setCharacters([...characters, newUser]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
